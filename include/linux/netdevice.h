0c22318e5e13353413e0684e5d01eab3637dec9d
Liudaozhihua <209014837@qq.com>
2025-11-15 14:11:23 +0000

    struct net_device: Use Android KABI Reserve for CONFIG_NET_L3_MASTER_DEV

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1974,7 +1974,7 @@ struct net_device {
 	const struct net_device_ops *netdev_ops;
 	const struct ethtool_ops *ethtool_ops;
 #ifdef CONFIG_NET_L3_MASTER_DEV
-	const struct l3mdev_ops	*l3mdev_ops;
+//	const struct l3mdev_ops	*l3mdev_ops;
 #endif
 #if IS_ENABLED(CONFIG_IPV6)
 	const struct ndisc_ops *ndisc_ops;
@@ -2228,8 +2228,12 @@ struct net_device {
 	ANDROID_KABI_RESERVE(4);
 	ANDROID_KABI_RESERVE(5);
 	ANDROID_KABI_RESERVE(6);
-	ANDROID_KABI_RESERVE(7);
-	ANDROID_KABI_RESERVE(8);
+    ANDROID_KABI_RESERVE(7);
+#if defined(CONFIG_NET_L3_MASTER_DEV)
+    ANDROID_KABI_USE(8, const struct l3mdev_ops	*l3mdev_ops;);
+#else
+    ANDROID_KABI_RESERVE(8);
+#endif
 };
 #define to_net_dev(d) container_of(d, struct net_device, dev)
 
